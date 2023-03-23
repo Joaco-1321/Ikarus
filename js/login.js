286314bf52d66c4f9e370225b826a9331fe1c6a1
Koki <KokiKyle13@gmail.com>
2023-03-23 17:23:18 +0100

    añadida maqueta login

---
+++ js/login.js
@@ -0,0 +1,19 @@
+const login = document.querySelector("#login"),
+  registrarse = document.querySelector("#registrarse");
+
+let bit = false;
+
+console.log(login);
+console.log(registrarse);
+
+function cambio() {
+	if (bit) {
+		login.style.display = 'flex';
+		registrarse.style.display = 'none';
+		bit = !bit;
+	} else {
+		registrarse.style.display = 'flex';
+		login.style.display = 'none';
+		bit = !bit;
+	}
+}
