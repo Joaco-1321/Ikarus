1d43629c01cb5242976305e04ea0ef3a50a2ef2b
Koki <KokiKyle13@gmail.com>
2023-04-21 12:48:20 +0200

    reorganización

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
     <meta name="keywords" content="Videojuegos, Steam, Periféricos, ">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de videojuegos</title>
-    <link type="text/css" rel="stylesheet" href="./../css/style.css">
-    <link rel="stylesheet" href="./../css/fontawesome/css/all.css">
+    <link type="text/css" rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/fontawesome/css/all.css">
     <link rel="shortcut icon" href="#" type="image/x-icon">
 </head>
 
 <body>
     <nav class="contenedor">
         <div class="contenedor" id="miniHeader">
-            <a href="index.html" class="contenedor">
+            <a href="index.php" class="contenedor">
                 <img src="">
                 <h1>Tienda de videojuegos</h1>
             </a>
@@ -25,7 +25,7 @@
             </form>
             <div class="contenedor" id="usuarioCarrito">
                 <a href="#" class="contenedor"><i class="fas fa-shopping-bag"></i></a>
-                <a href="login.html" class="contenedor">
+                <a href="pages/login.html" class="contenedor">
                     <i class="fas fa-user"></i>
                     <p>Usuario</p>
                 </a>
@@ -33,17 +33,17 @@
         </div>
         <div class="contenedor" id="miniNav">
             <ul class="contenedor">
-                <a href="perifericos.html" class="contenedor">
+                <a href="pages/perifericos.html" class="contenedor">
                     <li>Periféricos <i class="fas fa-headset"></i></i></li>
                 </a>
-                <a href="videojuegos.html" class="contenedor">
+                <a href="pages/videojuegos.html" class="contenedor">
                     <li>Videojuegos <i class="fas fa-gamepad"></i></li>
                 </a>
             </ul>
         </div>
     </nav>
     <header>
-        <img src="./../img/Banner.jpg" alt="">
+        <img src="img/Banner.jpg" alt="">
     </header>
     <main>
         <section>
@@ -101,7 +101,7 @@
             </div>
         </div>
         <div class="contenedor" id="contacto">
-            <h5><a href="contacto.html">Contáctanos</a></h5>
+            <h5><a href="pages/contacto.html">Contáctanos</a></h5>
         </div>
         <div class="contenedor" id="avisos">
             <span><a href="">Aviso legal</a>, <a href="">Términos y condiciones</a>, <a href="">Privacidad</a>, <a
