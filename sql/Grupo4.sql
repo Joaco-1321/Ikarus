16609b31486f57b898bfc91e74b28fdf27cab08e
Koki <KokiKyle13@gmail.com>
2023-04-25 10:51:46 +0200

    sql modificado

--- sql/Grupo4.sql
+++ sql/Grupo4.sql
@@ -1,83 +1,86 @@
-drop database if exists tiendavideojuegos;
-create database if not exists tiendavideojuegos;
-use tiendavideojuegos;
+DROP DATABASE IF EXISTS ikarus;
+CREATE DATABASE IF NOT EXISTS ikarus; 
+USE ikarus;
 
-create table if not exists Cliente (
-	id int not null auto_increment,
-    DNI varchar(10) not null,
-    Email varchar(45) not null,
-    Usuario varchar(45) not null,
-    Contraseña varchar(45) not null,
-    Nombre varchar(45),
-    Apellidos varchar(45),
-    Móvil varchar(45),
-    primary key(id),
-    unique(DNI),
-    unique(Email),
-    unique(Usuario)
-)engine = InnoDB; 
+CREATE TABLE IF NOT EXISTS rol (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(20) NOT NULL UNIQUE
+)ENGINE = INNODB;
 
-create table if not exists Pedido (
-	id int not null auto_increment,
-    `Fecha de realización` datetime not null,
-    `Fecha de envío` date,
-    Clientes_id int not null,
-    primary key(id),
-    foreign key(Clientes_id) references Cliente(id)
-)engine = InnoDB; 
+CREATE TABLE IF NOT EXISTS usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	usuario VARCHAR(45) NOT NULL UNIQUE,
+	email VARCHAR(45) NOT NULL UNIQUE,
+	contrasena VARCHAR(45) NOT NULL,
+	rol_id INT NOT NULL,
+	nombre VARCHAR(45),
+	apellidos VARCHAR(45),
+	direccion VARCHAR(100),
+	movil VARCHAR(45),
+	FOREIGN KEY(rol_id) REFERENCES rol(id)
+)ENGINE = INNODB;
 
-create table if not exists Proveedor (
-	id int not null auto_increment,
-    CIF varchar(15) not null,
-    Nombre varchar(45) not null,
-    Dirección varchar(45),
-    Teléfono varchar(45),
-    primary key(id),
-    unique(CIF)
-)engine = InnoDB;
+CREATE TABLE IF NOT EXISTS pedido (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	fechaRealizacion DATETIME NOT NULL,
+	fechaEnvio DATE,
+	usuario_id INT NOT NULL,
+	FOREIGN KEY(usuario_id) REFERENCES usuario(id)
+)ENGINE = INNODB;
 
-create table if not exists Producto (
-	id int not null auto_increment,
-    Nombre varchar(45) not null,
-    Tipo enum('videojuego', 'periférico') not null,
-    Precio decimal(10, 2) not null,
-    Stock int not null,
-    Review int,
-    Proveedor_id int not null,
-    primary key(id),
-    foreign key(Proveedor_id) references Proveedor(id)
-)engine = InnoDB; 
+CREATE TABLE if NOT EXISTS proveedor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	CIF VARCHAR(15) NOT NULL UNIQUE,
+	nombre VARCHAR(45) NOT NULL,
+	dirección VARCHAR(45),
+	telefono VARCHAR(45)
+)ENGINE = INNODB;
 
-create table if not exists Pedido_has_Producto (
-	Pedido_id int not null,
-    Producto_id int not null,
-    Cantidad int not null,
-    foreign key(Pedido_id) references Pedido(id),
-    foreign key(Producto_id) references Producto(id),
-    primary key(Pedido_id, Producto_id)
-)engine = InnoDB;
+CREATE TABLE if NOT EXISTS producto (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(45) NOT NULL,
+	tipo ENUM('videojuego', 'periférico') NOT NULL,
+	precio DECIMAL(10, 2) NOT NULL,
+	stock INT NOT NULL,
+	review INT,
+	ruta VARCHAR(50) NOT NULL UNIQUE,
+	proveedor_id INT NOT NULL,
+	FOREIGN KEY(proveedor_id) REFERENCES proveedor(id)
+)ENGINE = INNODB;
 
-insert into Cliente value (null, "123456799L", "pacosanz@gmail.com", "pacosanz", "paquito123", "Paco", "Sanz", 123456799);
-insert into Cliente value (null, "123456788L", "lauragimenez@gmail.com", "lauragim", "gimenezz", "Laura", "Gimenez", 123456788);
-insert into Cliente value (null, "123456777L", "juanlotillas@gmail.com", "juantole", "lotillas", "Juan", "Lotillas", 123456777);
-insert into Cliente value (null, "123456778L", "josenogales@gmail.com", "jositonoga", "nogalesjose", "Jose", "Nogales", 123456778);
+CREATE TABLE if NOT EXISTS pedido_has_producto (
+	pedido_id INT NOT NULL,
+	producto_id INT NOT NULL,
+	cantidad INT NOT NULL,
+	FOREIGN KEY(Pedido_id) REFERENCES Pedido(id),
+	FOREIGN KEY(Producto_id) REFERENCES Producto(id),
+	PRIMARY KEY(Pedido_id, Producto_id)
+)ENGINE = INNODB;
 
-insert into Pedido value (null, "2022-08-27 13:15:20", "2022-09-03", 1);
-insert into Pedido value (null, "2022-09-10 17:45:35", "2022-09-15", 2);
-insert into Pedido value (null, "2022-09-05 09:25:40", "2022-09-07", 3);
-insert into Pedido value (null, "2022-08-30 21:03:26", "2022-09-03", 1);
+INSERT INTO rol VALUES(NULL, 'usuario');
+INSERT INTO rol VALUES(NULL, 'admin');
 
-insert into Proveedor value (null, "A692547853G4", "Game", "Islazul Madrid", 987654321);
-insert into Proveedor value (null, "R666886653U4", "PC Componentes", "Callo", 956843221);
-insert into Proveedor value (null, "Y692321113O4", "Electronicas", "Plaza Eliptica", 9624781354);
-insert into Proveedor value (null, "K676355853H4", "Media Markt", "Nasica", 9647863225);
+INSERT INTO usuario (usuario, email, contrasena, rol_id) VALUES ('JoaquinAdmin', 'joaquin@imf.com', 'joaquin1234', 2);
+INSERT INTO usuario (usuario, email, contrasena, rol_id) VALUES ('JonasAdmin', 'jonas@imf.com', 'jonas1234', 2);
+INSERT INTO usuario (usuario, email, contrasena, rol_id, nombre, apellidos, direccion, movil) VALUES ('userChulo', 'correoChulo@gmail.com', '12345678', 1, 'Jose', 'Nogales', 'calle chula, 2 Madrid', 123456789);
+INSERT INTO usuario (usuario, email, contrasena, rol_id, direccion) VALUES ('carlosRufiangel', 'carlosrufi@gmail.com', 'springboot', 1, 'casa de carlos');
 
-insert into Producto values (null, "Logitech", "Periferico", 24.99, 34, 7, 2);
-insert into Producto values (null, "GTA", "VideoJuego", 14.99, 56, 10, 1);
-insert into Producto values (null, "Elden Ring", "VideoJuego", 59.99, 67, 10, 3);
-insert into Producto values (null, "Corsair", "Periferico", 29.99, 49, 6, 2);
+INSERT INTO pedido (fechaRealizacion, fechaEnvio, usuario_id) VALUES (NOW(), '2023-04-22', 1);
+INSERT INTO pedido (fechaRealizacion, fechaEnvio, usuario_id) VALUES (NOW(), '2023-04-23', 1);
+INSERT INTO pedido (fechaRealizacion, usuario_id) VALUES (NOW(), 2);
+INSERT INTO pedido (fechaRealizacion, usuario_id) VALUES (NOW(), 2);
 
-insert into Pedido_has_Producto values (3, 1, 3);
-insert into Pedido_has_Producto values (4, 2, 2);
-insert into Pedido_has_Producto values (1, 4, 3);
-insert into Pedido_has_Producto values (2, 3, 1);
+INSERT INTO proveedor VALUES (null, 'A692547853G4', 'Game', 'Islazul Madrid', 987654321);
+INSERT INTO proveedor VALUES (null, 'R666886653U4', 'PC Componentes', 'Callo', 956843221);
+INSERT INTO proveedor VALUES (null, 'Y692321113O4', 'Electronicas', 'Plaza Eliptica', 9624781354);
+INSERT INTO proveedor VALUES (null, 'K676355853H4', 'Media Markt', 'Nasica', 9647863225);
+
+INSERT INTO producto (nombre, tipo, precio, stock, proveedor_id, ruta) VALUES ('Logitech', 'Periferico', 24.99, 34, 2, 'img/logitech.png');
+INSERT INTO producto (nombre, tipo, precio, stock, proveedor_id, ruta) VALUES ('GTA V', 'VideoJuego', 14.99, 56, 1, 'img/GTAV.jpeg');
+INSERT INTO producto (nombre, tipo, precio, stock, review, proveedor_id, ruta) VALUES ('Elden Ring', 'VideoJuego', 59.99, 67, 10, 3, 'img/EldenRing.jpeg');
+INSERT INTO producto (nombre, tipo, precio, stock, review, proveedor_id, ruta) VALUES ('Corsair', 'Periferico', 29.99, 49, 6, 2, 'img/corsair.jpeg');
+
+INSERT INTO pedido_has_producto VALUES (3, 1, 3);
+INSERT INTO pedido_has_producto VALUES (4, 2, 2);
+INSERT INTO pedido_has_producto VALUES (1, 4, 3);
+INSERT INTO pedido_has_producto VALUES (2, 3, 1);
