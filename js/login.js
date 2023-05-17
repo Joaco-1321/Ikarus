ed8602fa54b30270e37ce36bcb83db34b5f27cda
Jonas <jonas@gmail.com>
2023-05-17 08:53:40 +0200

    cambioLogin

--- js/login.js
+++ js/login.js
@@ -15,6 +15,9 @@ function cambio() {
   }
 }
 
+let boton_enviar = document.getElementById('botonEnviar');
+boton_enviar.disabled = true;
+
 function contrasena(){
   let valuePass = document.getElementById("contraseña");
   let minus = document.getElementById("minuscula");
@@ -27,44 +30,54 @@ function contrasena(){
   if(valuePass.value.match(minusculas)){
     minus.classList.remove("incorrecto");
     minus.classList.add("correcto");
+    boton_enviar.disabled = false;
   } else {
     minus.classList.remove("correcto");
     minus.classList.add("incorrecto");
+    boton_enviar.disabled = true;
   }
 
   let mayusculas = /[A-Z]/g;
   if(valuePass.value.match(mayusculas)){
     mayus.classList.remove("incorrecto");
     mayus.classList.add("correcto");
+    boton_enviar.disabled = false;
   } else {
     mayus.classList.remove("correcto");
     mayus.classList.add("incorrecto");
+    boton_enviar.disabled = true;
   }
 
   let digito = /\d{2}/g;
   if(valuePass.value.match(digito)){
     digitos.classList.remove("incorrecto");
     digitos.classList.add("correcto");
+    boton_enviar.disabled = false;
   } else {
     digitos.classList.remove("correcto");
     digitos.classList.add("incorrecto");
+    boton_enviar.disabled = true;
   }
 
   let signoss = /[-.+_@]/g;
   if(valuePass.value.match(signoss)){
     signos.classList.remove("incorrecto");
     signos.classList.add("correcto");
+    boton_enviar.disabled = false;
   } else {
     signos.classList.remove("correcto");
     signos.classList.add("incorrecto");
+    boton_enviar.disabled = true;
   }
   
   if(valuePass.value.length >= 10){
     longitud.classList.remove("incorrecto");
     longitud.classList.add("correcto");
+    boton_enviar.disabled = false;
   } else {
     longitud.classList.remove("correcto");
     longitud.classList.add("incorrecto");
+    boton_enviar.disabled = true;
   }
 
 }
