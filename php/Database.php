0102f606fc7ff39360ad048e953463b921cb0e89
Koki <KokiKyle13@gmail.com>
2023-05-18 11:02:37 +0200

    Database mejorada

--- php/Database.php
+++ php/Database.php
@@ -1,39 +1,66 @@
 <?php
 class Database
 {
-	private static $dbh;
+  public static function conectar()
+  {
+    $drvr = 'mysql';
+    $db = 'ikarus';
+    $host = 'localhost';
+    $user = 'root';
+    $pswd = '';
 
-	public function conectar($user, $pswd)
-	{
-		if (!(static::$dbh instanceof PDO)) {
-			$drvr = 'mysql';
-			$db = 'ikarus';
-			$host = 'localhost';
+    $dns = "$drvr:dbname=$db;host=$host";
 
-			$dns = "$drvr:dbname=$db;host=$host";
+    try {
+      $dbh = new PDO($dns, $user, $pswd);
+      // echo 'conectado correctamente';
+    } catch (PDOException $e) {
+      echo 'falló la conexión' . $e->getMessage();
+    }
+    return $dbh;
+  }
 
-			try {
-				static::$dbh = new PDO($dns, $user, $pswd);
-			} catch (PDOException $e) {
-				echo 'falló la conexión' . $e->getMessage();
-			}
-		}
-		return static::$dbh;
-	}
+  public static function getAll($tabla)
+  {
+    $sql = "SELECT * FROM $tabla";
+    return self::conectar()->query($sql);
+  }
 
-	public function getAll($tabla)
-	{
-		$sql = 'SELECT * FROM ' . $tabla;
-		return static::$dbh->query($sql);
-	}
+  public static function findById($id)
+  {
+    $sql = "SELECT * FROM produco WHERE id = $id";
+    $ordenador = self::conectar()->query($sql);
+    return $ordenador->fetch(PDO::FETCH_ASSOC);
+  }
 
-	public function getVideojuegos() {
-		$sql = 'SELECT nombre, precio, ruta FROM producto WHERE tipo=\'videojuego\'';
-		return static::$dbh->query($sql);
-	}
+  public static function save($arr)
+  {
+    $sql = "INSERT INTO productos(nombre, tipo, precio, stock, review, ruta) VALUES('$arr[0]', '$arr[1]', $arr[2], '$arr[3]')";
+    return self::conectar()->query($sql);
+  }
 
-	public function getPerifericos() {
-		$sql = 'SELECT nombre, precio, ruta FROM producto WHERE tipo=\'periferico\'';
-		return static::$dbh->query($sql);
-	}
+  public static function update($arr)
+  {
+    $sql = "UPDATE ordenadores SET marca = '$arr[1]', modelo = '$arr[2]', precio = $arr[3], descripcion = '$arr[4]' WHERE id = $arr[0]";
+    return self::conectar()->query($sql);
+  }
+
+  public static function delete($id)
+  {
+    $sql = "DELETE FROM ordenadores WHERE id = $id";
+    return self::conectar()->query($sql);
+  }
+
+  public static function login($email, $password)
+  {
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+
+    $user = self::conectar()->query($sql);
+
+    if ($user != null) {
+      return $user->fetch(PDO::FETCH_ASSOC);
+    } else {
+      return null;
+    }
+  }
 }
